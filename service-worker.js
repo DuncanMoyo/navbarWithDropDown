2215debfd7e6a75dc1aa3f4d541371f87fc4d18f
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-15 23:21:24 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/http//:DuncanMoyo.github.io/navbarWithDropDown/precache-manifest.2e66308849a042bbfe6e0732ca458118.js"
+  "/navbarWithDropDown/precache-manifest.22cce00454bd1c83718c673989ba1889.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http//:DuncanMoyo.github.io/navbarWithDropDown/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/navbarWithDropDown/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
