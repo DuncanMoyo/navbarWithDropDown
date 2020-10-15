48eeb1db72fb83c770edb3c743ba664ad7b77c22
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-15 23:51:54 +0200

    Updates

---
+++ precache-manifest.a59fa5ded8d13bd4b5d4235261c7074a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8d3b51cc4bc4186d28aea1064a7c0e",
+    "url": "/navbarWithDropDown/index.html"
+  },
+  {
+    "revision": "fa83245d20f69d05a5ac",
+    "url": "/navbarWithDropDown/static/css/main.5065cb06.chunk.css"
+  },
+  {
+    "revision": "9debbc425785f2077c5e",
+    "url": "/navbarWithDropDown/static/js/2.8e05febc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/navbarWithDropDown/static/js/2.8e05febc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa83245d20f69d05a5ac",
+    "url": "/navbarWithDropDown/static/js/main.380823c5.chunk.js"
+  },
+  {
+    "revision": "86bda1c40f9071c0d98c",
+    "url": "/navbarWithDropDown/static/js/runtime-main.f9873f6c.js"
+  },
+  {
+    "revision": "cc2d7986609416edd7fccd8e427ccf06",
+    "url": "/navbarWithDropDown/static/media/img-1.cc2d7986.jpg"
+  },
+  {
+    "revision": "49d1766c400ae1cd9e57c845adc9b47a",
+    "url": "/navbarWithDropDown/static/media/img-2.49d1766c.jpg"
+  },
+  {
+    "revision": "209affa5ee2617a1aaa401e09105f989",
+    "url": "/navbarWithDropDown/static/media/img-3.209affa5.jpg"
+  },
+  {
+    "revision": "49b07d93298b047e5026a15b79e7c1be",
+    "url": "/navbarWithDropDown/static/media/img-4.49b07d93.jpg"
+  },
+  {
+    "revision": "1752792c26f4e1e3b62856d91862bca4",
+    "url": "/navbarWithDropDown/static/media/img-5.1752792c.jpg"
+  },
+  {
+    "revision": "58d21140d9528e5374245a82a4ae9b28",
+    "url": "/navbarWithDropDown/static/media/img-6.58d21140.jpg"
+  },
+  {
+    "revision": "f33a136de385761260809066077030c4",
+    "url": "/navbarWithDropDown/static/media/img-7.f33a136d.jpg"
+  }
+]);
