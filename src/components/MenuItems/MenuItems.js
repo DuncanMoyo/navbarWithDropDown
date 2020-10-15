419bc19e3ea28a62aef09d5ed5cc76bd0ed54d2b
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-15 21:56:34 +0200

    React Navbar with Dropdown

---
+++ src/components/MenuItems/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+ 
+  {
+    title: "Consulting",
+    path: "/consulting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Design",
+    path: "/design",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Development",
+    path: "/development",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Marketing",
+    path: "/marketing",
+    cName: "dropdown-link",
+  },
+];
